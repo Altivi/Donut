90d25ec6db76655e70d723935004e25c7566f87a
Altivi <altivi.prog@gmail.com>
2016-05-31 17:19:24 +0300

    Setting the production mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
 
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'mighty-ravine-76891.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
