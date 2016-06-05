d2fd5e439fb34d03524e10e77c88744d74f656fb
Altivi <altivi.prog@gmail.com>
2016-06-06 00:14:06 +0300

    added AJAX signup forms

---
+++ app/assets/javascripts/registrations.js.coffee
@@ -0,0 +1,15 @@
+$ ->
+	$('#signup_form').on 'ajax:success', (e, data, status, xhr) ->
+		$('#signup_form').prepend('<div class="alert alert-notice">A confirmation link has been sent to your email address!</div>')
+
+	$('#signup_form').on 'ajax:error', (e, data, status, xhr) ->
+		labels = $("#signup_form").children('.form-group')
+		$('.alert-danger').remove()
+		if data.responseJSON.errors
+			$('#signup_form').prepend('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><ul></ul></div>')
+		$.each(data.responseJSON.errors, (field, msg) ->
+			$('.alert-danger ul').append('<li>' + capitalizeFirstLetter(field) + ' ' + msg + '</li>')
+
+		)
+capitalizeFirstLetter = (string) ->
+  string.charAt(0).toUpperCase() + string.slice(1)
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
  # before_action :configure_sign_up_params, only: [:create]
  before_action :configure_account_update_params, only: [:update, :profile_update]
-
+  respond_to :json
   # GET /resource/sign_up
   # def new
   #   super
@@ -10,6 +10,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   # def create
   #   super
+  # end
+
+  # def create
+  #   build_resource(sign_up_params)
+
+  #   resource.save
+  #   yield resource if block_given?
+  #   if resource.persisted?
+  #     if resource.active_for_authentication?
+  #       set_flash_message! :notice, :signed_up
+  #       sign_up(resource_name, resource)
+  #       respond_with resource, location: after_sign_up_path_for(resource)
+  #     else
+  #       set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+  #       expire_data_after_sign_in!
+  #       respond_with resource, location: after_inactive_sign_up_path_for(resource)
+  #     end
+  #   else
+  #     clean_up_passwords resource
+  #     set_minimum_password_length
+  #     case request.referrer
+  #     when 'http://localhost:3000/'
+  #       render 'static_pages/home'
+  #     else
+  #       respond_with resource
+  #     end
+  #   end
   # end
 
   # GET /resource/edit
@@ -55,7 +82,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
